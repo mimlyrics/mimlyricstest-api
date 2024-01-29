b5538eb4848fbe4ede3f8873aa641acfd881a67b
Mim <mimlyrics8@gmail.com>
2024-01-29 02:55:13 +0100

    Ready to deploy backend

--- config/db.js
+++ config/db.js
@@ -1,13 +1,23 @@
-const mongoose = require('mongoose');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(process.env.MONGO_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
 
 const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(process.env.MONGO_URI);
-        console.log(`Db connection successful to - ${conn.connection.host}`);
-        return conn;
-    }catch(error) {
-        console.error(`connection failed`);
-    }
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
 }
-
-module.exports = connectDB;
+module.exports = connectDB
